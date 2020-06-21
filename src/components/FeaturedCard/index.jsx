3302edb5c86440ef0d2b2dcce87e6fa45b85ae06
ashwin-pc <ashwinpc1993@gmail.com>
2020-06-21 03:11:44 -0700

    feat(Featured): Adds absolute URL support for background

--- src/components/FeaturedCard/index.jsx
+++ src/components/FeaturedCard/index.jsx
@@ -4,13 +4,18 @@ import { Card } from '../Card'
 import styles from './index.module.css'
 
 const FeaturedCard = ({ name, link, tags = '', background = '/assets/card.png' }) => {
-    const url = (process.env.PUBLIC_URL || '.') + background
+    const url = isAbsoluteURL(background) ? background : (process.env.PUBLIC_URL || '.') + background
     return (
         <Card link={link} className={styles.container} background={url} tag={[name,link,tags].join(' ')}>
             {name}
         </Card>
     )
-}  
+}
+
+function isAbsoluteURL(url) {
+    const pat = /^https?:\/\//i
+    return pat.test(url)
+}
 
 export {
     FeaturedCard,
