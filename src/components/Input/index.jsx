8bfa11c3eced843cbc241187a995690a4c0960b0
ashwin-pc <ashwinpc1993@gmail.com>
2020-07-02 17:51:29 -0700

    feat(Search): adds Keyboard navigation

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './index.module.css'
 
-export const Input = ({ name='input', label, value, onChange }) => {
+export const Input = ({ name='input', label, value, onChange, ...props }) => {
     return (
         <div className={styles.inputContainer}>
             { label && <label htmlFor="name" className={styles.label}>{label}</label> }
-            <input type="text" name={name} value={value} onChange={onChange} className={styles.input} />
+            <input type="text" name={name} value={value} onChange={onChange} className={styles.input} {...props} />
         </div>
     )
 }
